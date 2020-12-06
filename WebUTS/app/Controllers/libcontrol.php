02eecee25eb320811df91b82c40c3c2986771f47
jonathanmchl <jonathanmworks@gmail.com>
2020-12-06 18:21:41 +0700

    first

---
+++ WebUTS/app/Controllers/libcontrol.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class libcontrol extends BaseController
+{
+	public function index()
+	{
+		return view('index/index');
+	}
+
+	//--------------------------------------------------------------------
+
+}
---
+++ WebUTS/app/Views/index/footer.php
@@ -0,0 +1,7 @@
+ <!-- Copyright -->
+ <div class="footer-copyright text-center py-3">© 2020 Copyright: Bengal Uci JO
+   
+  </div>
+  <!-- Copyright -->
+
+</footer>
